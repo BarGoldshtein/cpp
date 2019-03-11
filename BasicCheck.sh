3e235b8af42c817d4820fc34ea567f7f5ae05c18
root <root@localhost.localdomain>
2019-03-11 07:28:45 -0400

    sds

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -8,22 +8,17 @@ if [ -f makefile ] || [ -f Makefile ] ;
 		make MAIN="$dir/greeting1.cpp"
 		if [ $? -eq 0 ]
 			then
-echo "1111111111111111111111111111111111111111111111ahsdkhashdfklshdflksdhklgwgljeflkdgjeflkdgjlfdjglkfjhlkfjhlkefdjhlkefjhlkefdjhlkef"
 			chmod 755 $program
 			valgrind --leak-check=full --error-exitcode=1 ./$program
 			result1=$?
 			valgrind --tool=helgrind --error-exitcode=1 ./$program
 			result2=$?
-##			clear
+			clear
 			if [ $result1 -eq 0 ]
 				then
-				echo "222222222222222222222222222222222222222222222222222222222"
-echo $result1
-printf"testtttttttttttttttttttttttttttttttt";
 				##result1 = 0
 				resultString1="PASS";
 			else
-echo "333333333333333333333333333333333333"
 				##result1 = 1
 				resultString1="FAILED"
 				result1=$(($result1*2));
@@ -31,28 +26,23 @@ echo "333333333333333333333333333333333333"
 			fi
 			if [ $result2 -eq 0 ]
 				then
-echo "444444444444444444444444444444444"
 				##result2 = 0
 				resultString2="PASS";
 			else
-echo "55555555555555555555555555555555"
 				##result2 = 1
 				resultString2="FAILED"
 			fi
-echo "666666666666666666666666"
 			printf "BasicCheck.sh $dir $program $args\n"
 			printf "Compilation\t  Memory leaks\t thread race\n"
 			printf "PASS\t\t $resultString1\t\t $resultString2\n"
 			exit $(($result1+$result2));
 		else
-echo "77777777777777777777777777"
 			printf "BasicCheck.sh $dir $program $args\n"
 			printf "Compilation\t Memory leaks\t thread race\n"
 			printf "Failed\t\t Failed\t\t Failed\n"
 			exit 7
 		fi
 	else
-echo "88888888888888888888888888888888888888"
 	echo "not found"
 	exit
 fi
